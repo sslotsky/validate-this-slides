5a06bdea18202417055e34c630388f62d688792e
Sam Slotsky <saxosamo@gmail.com>
2017-09-09 11:24:49 -0500

    Initial commit

---
+++ presentation/index.js
@@ -0,0 +1,243 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import {
+  BlockQuote,
+  CodePane,
+  Cite,
+  Deck,
+  Fill,
+  Fit,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  ListItem,
+  List,
+  Notes,
+  Quote,
+  Slide,
+  Text
+} from "spectacle";
+
+// Import image preloader util
+import preloader from "spectacle/lib/utils/preloader";
+
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+
+const theme = createTheme({
+  primary: "white",
+  secondary: "#1F2022",
+  tertiary: "#03A9FC",
+  quartenary: "#CECECE"
+}, {
+  primary: "Montserrat",
+  secondary: "Helvetica"
+});
+
+// Require CSS
+require("normalize.css");
+require("spectacle/lib/themes/default/index.css");
+require("../themes/seattlejs/index.css");
+// Best way to include fonts rite
+require("../fonts/worksans.css");
+require("../fonts/biorhyme.css");
+require("../fonts/silkscreen.css");
+
+const images = {
+  skyline: require("../assets/skyline.jpg"),
+  walken: require("../assets/walken.jpg"),
+  samo: require("../assets/samo.jpg"),
+  flex: require("../assets/flex.jpg")
+};
+
+preloader(images);
+
+const builtIns = `
+function validate(values) {
+  return validator(values, form => {
+    form.validate('username', 'password', 'confirm').required()
+    form.validate('confirm').matches('password')
+    form.validate('gpa').isNumeric()
+  })
+}
+`;
+
+const translations = `
+function validate(values) {
+  return validator(values, form => {
+    form.validate('username', 'password', 'confirm').required()
+    form.validate('confirm').matches('password')
+  }, (message, field) => I18n.t(\`forms.newUser.\${field}.\${message}\`))
+}
+`;
+
+const nested = `
+function validate(values) {
+  return validator(values, form => {
+    form.validateChild('address', address => {
+      address.validate('street').required()
+    })
+
+    form.validateChildren('contacts', contact => {
+      contact.validate('name', 'email').required()
+    })
+  })
+}
+`;
+
+const nestedTranslations = `
+function validate(values) {
+  return validator(values, form => {
+    form.validate('username', 'password', 'confirm').required()
+    form.validate('confirm').matches('password')
+
+    form.validateChild('address', address => {
+      address.validate('street').required()
+    }, (message, field) => I18n.t(\`forms.newUser.address.\${field}.\${message}\`))
+  }, (message, field) => I18n.t(\`forms.newUser.\${field}.\${message}\`))
+}
+`;
+
+const theGoodWay = `
+defineValidator({
+  name: 'required',
+  rule: value => {
+    if (!value) {
+      return 'Cannot be blank, dude'
+    }
+  }
+})
+`;
+
+const theBetterWay = `
+function greaterThan(n) {
+  return value => {
+    if (value <= n) {
+      return 'too_small'
+    }
+  }
+}
+ 
+function lessThan(n) {
+  return value => {
+    if (value >= n) {
+      return 'too_big'
+    }
+  }
+}
+ 
+function validate(values) {
+  return validator(values, form => {
+    form.validate('age').satisfies(greaterThan(17), lessThan(26))
+  })
+}
+`;
+
+export default class Presentation extends React.Component {
+  renderSponsorHeading(text) {
+    return (
+      <Heading
+        caps
+        size={6}
+        style={{ letterSpacing: "0.05em" }}
+      >
+        {text}
+      </Heading>
+    );
+  }
+
+  background(image) {
+    return  {
+      backgroundImage: `url(${image})`,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "top center",
+      backgroundSize: "105% auto",
+      color: 'white'
+    };
+  }
+
+  render() {
+    const skylineBg = this.background(images.skyline);
+
+    const walkenBg = this.background(images.walken);
+
+    const samoBg = this.background(images.samo);
+
+    return (
+      <Deck
+        progress="none"
+        theme={theme}
+        transition={["fade"]}
+        transitionDuration={500}
+      >
+        <Slide style={skylineBg}>
+          <div style={{ backgroundColor: 'rgba(0, 0, 0, .35)', padding: '20px' }}>
+            <Heading size={1} textColor="primary">React MPLS</Heading>
+            <Text textFont="monospace" margin="auto auto 5em auto" textColor="primary">
+              Sept 21, 2017
+            </Text>
+            <Heading size={4} textColor="primary">Validate this, that, and the other thing</Heading>
+          </div>
+        </Slide>
+        <Slide style={walkenBg}>
+          <BlockQuote>
+            <Quote>Does your mother validate? Tell her to validate this!</Quote>
+            <Cite textColor="primary">Christopher Walken (but not really)</Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide>
+          <Heading size={3} textColor="secondary">Sam Slotsky</Heading>
+          <Layout>
+            <Fit>
+              <Image height={300} width={300} src={images.samo} />
+            </Fit>
+            <Fill>
+              <List style={{ listStyle: 'none', paddingLeft: '20px' }}>
+                <ListItem>Lucky husband</ListItem>
+                <ListItem>Proud dad</ListItem>
+                <ListItem>Jazz saxophonist</ListItem>
+                <ListItem>Professional JavaScript nerd</ListItem>
+                <ListItem>Really, really bad at slides</ListItem>
+              </List>
+            </Fill>
+          </Layout>
+        </Slide>
+        <Slide>
+          <Heading size={3} textColor="secondary">Built In Validations</Heading>
+          <Heading size={5} textColor="secondary">(we only have a few)</Heading>
+          <CodePane lang="javascript" source={builtIns} />
+        </Slide>
+        <Slide>
+          <Heading size={3} textColor="secondary">Message Translation</Heading>
+          <CodePane lang="javascript" source={translations} />
+        </Slide>
+        <Slide>
+          <Heading size={3} textColor="secondary">Nested Objects &amp; Arrays</Heading>
+          <CodePane lang="javascript" source={nested} />
+        </Slide>
+        <Slide>
+          <Heading size={3} textColor="secondary">Nested Translations</Heading>
+          <CodePane lang="javascript" source={nestedTranslations} />
+        </Slide>
+        <Slide>
+          <Heading size={3} textColor="secondary">Custom Validations</Heading>
+          <Heading size={5} textColor="secondary">For Better Flexibility</Heading>
+          <Image src={images.flex} />
+        </Slide>
+        <Slide>
+          <Heading size={3} textColor="secondary">The Good Way</Heading>
+          <Heading size={5} textColor="secondary">Create your own built-in rules</Heading>
+          <CodePane lang="javascript" source={theGoodWay} />
+        </Slide>
+        <Slide>
+          <Heading size={3} textColor="secondary">The Better Way</Heading>
+          <Heading size={5} textColor="secondary">Satisfy any condition</Heading>
+          <CodePane lang="javascript" source={theBetterWay} />
+        </Slide>
+      </Deck>
+    );
+  }
+}
